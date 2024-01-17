af776227a43ed8804816d5b9d72b0252bae69f98
Atul <atulst825@gmail.com>
2024-01-17 20:14:28 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/atulranjan9026",
   resume:
-    "https://drive.google.com/file/d/12190mM2TtLNQ-rQcu8aU4mY843FMyybp/view?usp=sharing",
+    "https://drive.google.com/file/d/1dEoBourRCJRZjI9YM8BpYABaKkmNpf4S/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/atul-ranjan-50489921a/",
   twitter: "https://twitter.com/atulst825",
   insta: "https://www.instagram.com/atulranjan3625/?hl=en",

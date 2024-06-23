ed29f37fb5c443b8eee1d24fc76ea80e44cf8ba2
Atul <atulst825@gmail.com>
2024-06-23 17:28:15 +0530

    link

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/atulranjan9026",
   resume:
-    "https://drive.google.com/file/d/1f82ZUY6TYg_ECw6jU9050VGlL-mKCcSZ/view?usp=sharing",
+    "https://drive.google.com/file/d/1ujjOA5FVVgmlHmk8HhZZAgyREWop4J1_/view",
   linkedin: "https://www.linkedin.com/in/atul-ranjan-50489921a/",
   twitter: "https://twitter.com/atulst825",
   insta: "https://www.instagram.com/atulranjan3625/?hl=en",

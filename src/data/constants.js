0b7797db3c3b7a2d711c234279e09a4b6f89d615
Atul Ranjan <99653239+atulranjan9026@users.noreply.github.com>
2024-07-17 16:15:39 +0530

    Update Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/atulranjan9026",
   resume:
-    "https://drive.google.com/file/d/1x5yEp4GHaLE_aqJvdnxSrMFtgNAyOK57/view?usp=sharing",
+    "https://drive.google.com/drive/folders/1HHEsEWXPR9m0z0eB-dAr2SBzyD0r0nX7",
   linkedin: "https://www.linkedin.com/in/atul-ranjan-50489921a/",
   twitter: "https://twitter.com/atulst825",
   insta: "https://www.instagram.com/atulranjan3625/?hl=en",
